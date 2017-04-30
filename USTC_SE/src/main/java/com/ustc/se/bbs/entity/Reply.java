3ddd656d2f82591942ec8505b652ca84b707e826
Administrator <Administrator@192.168.47.1>
2017-04-30 20:00:21 +0800

    add reply

---
+++ USTC_SE/src/main/java/com/ustc/se/bbs/entity/Reply.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.ustc.se.bbs.entity;
+
+/**
+ * .java 
+ * @author 
+ * @CreatTime 2017年4月30号
+ * @Updater zlf1993
+ * @UpdateTime 2917年4月30号
+ *
+ */
+public class Reply {
+
+}
